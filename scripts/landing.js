ff5f66a63977dbaaad273da583e703ee642bf20e
Stephen Coyne <scoyne@bw.edu>
2017-09-21 20:22:24 -0400

    updated assignment 8 DOM

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,10 @@
-    var revealPoints = function() {
-        var points = document.getElementsByClassName('point');
-        
-        for(var i = 0; i < points.length; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
+var revealPoints = function() {
+    var points = document.getElementsByClassName('point');
+
+    for(var i = 0; i < points.length; i++) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     };
+};
