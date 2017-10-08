a369ae2c3ab1d2b9d87492b47bfef43aedf3ff6a
Stephen Coyne <scoyne@bw.edu>
2017-10-08 15:11:40 -0400

    updated completed checkpoint-13-DOM-playpause-2

--- scripts/album.js
+++ scripts/album.js
@@ -85,13 +85,13 @@ var currentlyPlayingSong = null;
 window.onload = function() {
 
     var findParentByClassName = function(element, targetClass) {
-        if (element) {
+      if (element) {
             var currentParent = element.parentElement;
             while (currentParent.className !== targetClass && currentParent.className !== null) {
                 currentParent = currentParent.parentElement;
             }
             return currentParent;
-        }
+          }
       };
 
     var getSongItem = function(element) {
