93908144d5441fbec29cd117b183f13799cb9ea1
Stephen Coyne <scoyne@bw.edu>
2017-10-23 20:08:51 -0400

    completed checkpoint-19-jQuery-nextPrevious

--- scripts/album.js
+++ scripts/album.js
@@ -1,54 +1,3 @@
-// Example Album 1
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
-     ]
-};
-
-// Example Album 2
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-       { title: 'Hello, Operator?', duration: '1:01' },
-       { title: 'Ring, ring, ring', duration: '5:01' },
-       { title: 'Fits in your pocket', duration: '3:21'},
-       { title: 'Can you hear me now?', duration: '3:14' },
-       { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
-
-// Example Album 3
-var albumVila = {
-    title: 'The House',
-    artist: 'Robert Vila',
-    label: 'EM',
-    year: '1920',
-    albumArtUrl: 'assets/images/album_covers/21.png',
-    songs: [
-        { title: 'Knock knock knocking on the door', duration: '11:01' },
-        { title: 'Walk walk walking on the floor', duration: '3:01' },
-        { title: 'Cooking in the kitchen', duration: '3:21'},
-        { title: 'Living room couch', duration: '3:30' },
-        { title: 'Sleeping in a bed', duration: '1:15'},
-        { title: 'Kitchen table coding', duration: '5:15'},
-        { title: 'Drafty window', duration: '2:15'},
-        { title: 'Epic guitar solo', duration: '30:15'}
-    ]
-};
-
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
@@ -62,24 +11,41 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     var clickHandler = function() {
 
-    }
+        var songNumber = parseInt($(this).attr('data-song-number'));
+
+        if (currentlyPlayingSongNumber !== null) {
+            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+            currentlyPlayingCell.html(currentlyPlayingSongNumber);
+        }
+
+        if (currentlyPlayingSongNumber !== songNumber) {
+            $(this).html(pauseButtonTemplate);
+            currentlyPlayingSongNumber = songNumber;
+            currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+            updatePlayerBarSong();
+        } else if(currentlyPlayingSongNumber === songNumber) {
+            $(this).html(playButtonTemplate);
+            $('.main-controls .play-pause').html(playerBarPlayButton);
+            currentlyPlayingSongNumber = null;
+            currentSongFromAlbum = null;
+        }
+    };
 
     var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('.data-song-number');
-
-        if(songNumber !== currentlyPlayingSong) {
-            songNumberCell.html(playButtonTemplate)
+        var songNumber = songNumberCell.attr('data-song-number');
+        if (songNumber !== currentlyPlayingSongNumber) {
+            songNumberCell.html(playButtonTemplate);
         }
     };
 
     var offHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('.data-song-number');
-
-        if(songNumber !== currentlyPlayingSong) {
+        var songNumber = songNumberCell.attr('data-song-number');
+        if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
         }
+        console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
     };
 
     $row.find('.song-item-number').click(clickHandler);
@@ -88,6 +54,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 };
 
 var setCurrentAlbum = function(album) {
+    currentAlbum = album;
     var $albumTitle = $('.album-view-title');
     var $albumArtist = $('.album-view-artist');
     var $albumReleaseInfo = $('.album-view-release-info');
@@ -107,22 +74,75 @@ var setCurrentAlbum = function(album) {
     }
  };
 
+ var trackIndex = function(album, song) {
+    return album.songs.indexOf(song);
+};
+
+var nextSong = function() {
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+        currentSongIndex++;
+
+    if(currentSongIndex >= currentAlbum.songs.length) {
+        currentSongIndex = 0;
+    }
+
+     var lastSongNumber = currentlyPlayingSongNumber;
+
+     currentlyPlayingSongNumber = currentSongIndex + 1;
+     currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+     updatePlayerBarSong();
+
+     var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+     var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+     $nextSongNumberCell.html(pauseButtonTemplate);
+     $lastSongNumberCell.html(lastSongNumber);
+};
+
+var previousSong = function() {
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    currentSongIndex--;
+
+    if(currentSongIndex < 0) {
+        currentSongIndex = currentAlbum.songs.length - 1;
+    }
+
+    var lastSongNumber = currentlyPlayingSongNumber;
+
+    currentlyPlayingSongNumber = currentSongIndex + 1;
+    currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+    updatePlayerBarSong();
+
+    $('.main-controls .play-pause').html(playerBarPauesButton);
+
+    var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+    $previousSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+};
+
+var updatePlayerBarSong = function() {
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + ' - ' + currentAlbum.title);
+    $('.main-controls .play-pause').html(playerBarPauesButton);
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-var currentlyPlayingSong = null;
+var playerBarPlayButton = '<span class="ion-play"></span>';
+var playerBarPauesButton = '<span class="ion-pause"></span>';
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
 
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
-
-    var allAlbums = [albumPicasso, albumMarconi, albumVila];
-    var index = 1;
-    /*
-    $albumImage.addEventListener('click', function(event) {
-        setCurrentAlbum(allAlbums[i]);
-        index++
-        if(index == allAlbums.length) {
-            index = 0;
-        }
-    });
-    */
+    $previousButton.click(previousSong);
+    $nextButton.click(nextSong);
 });
---
+++ scripts/fixtures.js
@@ -0,0 +1,50 @@
+// Example Album 1
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+     ]
+};
+
+// Example Album 2
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+       { title: 'Hello, Operator?', duration: '1:01' },
+       { title: 'Ring, ring, ring', duration: '5:01' },
+       { title: 'Fits in your pocket', duration: '3:21'},
+       { title: 'Can you hear me now?', duration: '3:14' },
+       { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+// Example Album 3
+var albumVila = {
+    title: 'The House',
+    artist: 'Robert Vila',
+    label: 'EM',
+    year: '1920',
+    albumArtUrl: 'assets/images/album_covers/21.png',
+    songs: [
+        { title: 'Knock knock knocking on the door', duration: '11:01' },
+        { title: 'Walk walk walking on the floor', duration: '3:01' },
+        { title: 'Cooking in the kitchen', duration: '3:21'},
+        { title: 'Living room couch', duration: '3:30' },
+        { title: 'Sleeping in a bed', duration: '1:15'},
+        { title: 'Kitchen table coding', duration: '5:15'},
+        { title: 'Drafty window', duration: '2:15'},
+        { title: 'Epic guitar solo', duration: '30:15'}
+    ]
+};
